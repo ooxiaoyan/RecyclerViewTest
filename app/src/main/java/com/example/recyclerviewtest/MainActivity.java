642e98c9388dac12581b0436ad5b33c144b69531
Qian Wang <810978527@qq.com>
2018-09-10 14:30:50 +0800

    StaggeredGridLayout

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initFruits();
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
@@ -27,26 +30,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit("apple", R.drawable.apple_pic);
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
             fruitList.add(apple);
-            Fruit banana = new Fruit("banana", R.drawable.banana_pic);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
             fruitList.add(banana);
-            Fruit orange = new Fruit("orange", R.drawable.orange_pic);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
             fruitList.add(orange);
-            Fruit watermelon = new Fruit("watermelon", R.drawable.watermelon_pic);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
             fruitList.add(watermelon);
-            Fruit pear = new Fruit("pear", R.drawable.pear_pic);
+            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic);
             fruitList.add(pear);
-            Fruit grape = new Fruit("grape", R.drawable.grape_pic);
+            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic);
             fruitList.add(grape);
-            Fruit pineapple = new Fruit("pineapple", R.drawable.pineapple_pic);
+            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
             fruitList.add(pineapple);
-            Fruit strawberry = new Fruit("strawberry", R.drawable.strawberry_pic);
+            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
             fruitList.add(strawberry);
-            Fruit cherry = new Fruit("cherry", R.drawable.cherry_pic);
+            Fruit cherry = new Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic);
             fruitList.add(cherry);
-            Fruit mango = new Fruit("mango", R.drawable.mango_pic);
+            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic);
             fruitList.add(mango);
         }
+    }
+
+    //Ensure that the name of the fruit is different in length.
+    //the waterfall flow layout (StaggeredGridLayoutManager) needs the height difference of each sub item to see the obvious effect.
+    private String getRandomLengthName(String name) {
+
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;//create a random number between 1 to 20
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {//repeat the name (random number) times
+            builder.append(name);
+        }
+        return builder.toString();
     }
 }
